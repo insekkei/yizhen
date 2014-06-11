0c1485738d203709d31a650eced6ab196d35e406
hancongcs2c <cong.han@bj.cs2c.com.cn>
2014-06-11 16:24:41 +0800

    ...

--- js/qa.js
+++ js/qa.js
@@ -1,30 +1,51 @@
 $(document).ready(function(){
 	//获取qa列表
+	var url = 'http://115.29.143.102:8080/YizhenServer/que/getabstractlist?uid=18&token=9c6286f6155ba81948f0379ad3bb1257&currentqueindex=0&categaory=-1&client=web';
 	$.ajax({ 
-		url: YZAPI_GETURL('/que/getabstractlist?currentqueindex=0&categaory=-1'),
+		url: url,
 		type:"get",
 		dataType: "jsonp",
 		jsonpCallback: 'callback',
 		success: function(data){ 
 			var html = '';
-			console.info(data);
+			//console.info(data.abstractlist);
 			$.each(data.abstractlist, function(qaIndex, qa) {
 
 				html += '<li>';
 				html += '<p class="intro clearfix">'+getCat(qa.category)+'<span class="fs12 fr">'+qa.publishdate+'</span></p>';
-				html += '<p class="ques-title"><a href="#" onclick="return false;" qaid="' + qa.qaid + '" qeid="' + qa.qeid + '">'+qa.title+'</a></p>';
+				html += '<p class="ques-title"><a href="">'+qa.title+'</a></p>';
+				html += '<span class="hidden">'+qa.qeid+'</span>';
 				html += '</li>';
 			});
 			$('#ques').html(html);
 
 			//获取详情
 			$('.ques-title a').click(function(e){
-				var qaid = $(this).attr('qaid');
-				var qeid = $(this).attr('qeid');
-				//$('#qa').load(YZAPI_GETWeb('/page/quedetail.jsp?qaid='+qaid + "&qeid=" + qeid));
-				$('#qa-iframe').attr('src', YZAPI_GETWeb('/page/quedetail.jsp?qaid='+qaid + "&qeid=" + qeid));
-				$('#qa-div').hide();
-				$('#qa-iframe').show();
+			
+				var qeid = $(this).parent('p').next('.hidden');
+				url = 'http://115.29.143.102:8080/YizhenServer/que/getdetail?uid=18&token=9c6286f6155ba81948f0379ad3bb1257&qeid='+qeid;
+				$.ajax({ 
+					url: url,
+					type:"get",
+					dataType: "jsonp",
+					jsonpCallback: 'callback',
+					success: function(data){ 
+						var html = '';
+						console.info(data.abstractlist);
+						$.each(data.abstractlist, function(qaIndex, qa) {
+
+							html += '<li>';
+							html += '<p class="intro clearfix">'+getCat(qa.category)+'<span class="fs12 fr">'+qa.publishdate+'</span></p>';
+							html += '<p class="ques-title"><a href="">'+qa.title+'</a></p>';
+							html += '<span class="hidden">'+qa.qeid+'</span>';
+							html += '</li>';
+						});
+						$('#ques').html(html);
+					}, 
+					error: function(){ 
+						alert('网络异常'); 
+					} 
+				});
 			});//获取详情over
 		}, 
 		error: function(){ 
@@ -32,7 +53,7 @@ $(document).ready(function(){
 		} 
 	});
 
-	
+
 })
 
 //获取分类
